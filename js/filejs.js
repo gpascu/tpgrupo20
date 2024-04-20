bfd9b1c627ec7fcea063b908566020cdd7806e75
gpascu <pascuzzige@gmail.com>
2024-04-20 07:35:52 -0300

    Activaciòn menu H y cambio estilo

---
+++ js/filejs.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('ul');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show-menu');
+        menuToggle.classList.toggle('active');
+    });
+
+    // Cierra el menu al elegir
+    const menuItems = document.querySelectorAll('ul li a');
+    menuItems.forEach(item => {
+        item.addEventListener('click', function() {
+            menu.classList.remove('show-menu');
+            menuToggle.classList.remove('active');
+        });
+    });
+
+    // Cierra el menu al desplazar
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.addEventListener('wheel', function() {
+            menu.classList.remove('show-menu');
+            menuToggle.classList.remove('active');
+        });
+    });
+});
