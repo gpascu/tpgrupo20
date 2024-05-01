3f0706bea73741259e3c68837ee79a243edbd886
gpascu <pascuzzige@gmail.com>
2024-05-01 16:57:04 +0200

    modif H y M

--- js/filejs.js
+++ js/filejs.js
@@ -26,5 +26,46 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-
+//control formulario de contacto
+document.addEventListener('DOMContentLoaded', () => {
+    const submitButton = document.querySelector('input[type="submit"]');
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const mensajeInput = document.querySelector('#mensaje');
+  
+    submitButton.addEventListener('click', (e) => {
+      e.preventDefault(); 
+  
+      const nombreValue = nombreInput.value.trim();
+      const emailValue = emailInput.value.trim();
+      const mensajeValue = mensajeInput.value.trim();
+  
+      if (!nombreValue ||!emailValue ||!mensajeValue) {
+        alert('Completa los campos correctamente');
+        return;
+      }
+  
+      if (!/^[a-zA-Z\s]+$/.test(nombreValue)) {
+        alert('El nombre solo debe contener letras y espacios');
+        return;
+      }
+  
+      if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailValue)) {
+        alert('El email debe contener @ y punto');
+        return;
+      }
+  
+      if (mensajeValue.length < 1) {
+        alert('El mensaje debe contener al menos un caracter');
+        return;
+      }
+  
+      setTimeout(() => {
+        alert('Datos enviados');
+        nombreInput.value = '';
+        emailInput.value = '';
+        mensajeInput.value = '';
+        window.location.href = 'index.html'; // recarga index.html 
+      }, 4000); // muestra el alerta por 4 segundos
+    });
+  });
